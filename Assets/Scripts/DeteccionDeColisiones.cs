6921897b7f00dfaeb68b47bc19d725e90d37e593
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-05-22 10:40:10 -0300

    todo

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision col) // bege
+    {
+        Debug.Log("Contacto");
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
